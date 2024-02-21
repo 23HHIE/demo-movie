f1cc04203d6a14fb7d280a002f86929a51c120d6
EC2 Default User <ec2-user@ip-172-31-13-203.eu-west-1.compute.internal>
2024-02-21 13:24:29 +0000

    TO CHECK IF THE PIPELINE WORKS

--- demoproj/urls.py
+++ demoproj/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# A comment to check if the connection between the github and the pipelind works
 from django.contrib import admin
 from django.urls import path, include
 
