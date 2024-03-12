b3dfde241c42a44e6a1f03a77a37128eeb12ee35
EC2 Default User <ec2-user@ip-172-31-13-203.eu-west-1.compute.internal>
2024-03-12 15:22:55 +0000

    m2

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-""" A file automatically created in each Django project. """
+
 
 from django.http import Http404
 from django.shortcuts import render
@@ -9,6 +9,7 @@ from .models import Movie
 
 def index(request):
     newest_movies = Movie.objects.order_by('-release_date')[:15]
+    """ A file automatically created in each Django project. """
     context = {'newest_movies': newest_movies}
     return render(request, 'movies/index.html', context)
     
@@ -16,7 +17,8 @@ def index(request):
 def show(request, movie_id):
     try:
         movie = Movie.objects.get(pk=movie_id)
-    except Movie.DoesNotExist:
+        """ A file automatically created in each Django project. """
+    except Movie.DoesNotExist as exc:
         raise Http404("Bear it in mind, you might want to check the name of the movie")
     return render(request, 'movies/show.html', {'movie': movie})
 
